dfc70ce2d7a1d822676be558d17341f8cf5a9817
Mauro M <24578415+Bocanada@users.noreply.github.com>
2022-06-19 20:41:11 -0300

    impl SetQuestions method

--- server/tests.go
+++ server/tests.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"errors"
+	"io"
 
 	"github.com/bocanada/grpc/models"
 	"github.com/bocanada/grpc/repository"
@@ -33,4 +35,20 @@ func (s *TestServer) SetTest(ctx context.Context, req *testpb.Test) (*testpb.Set
 		return nil, err
 	}
 	return &testpb.SetTestResponse{Id: req.Id}, nil
+}
+
+func (s *TestServer) SetQuestions(stream testpb.TestService_SetQuestionsServer) error {
+	for {
+		msg, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return stream.SendAndClose(&testpb.SetQuestionResponse{Ok: true})
+		}
+		if err != nil {
+			return err
+		}
+		qa := &models.Question{Id: msg.Id, Question: msg.Question, Answer: msg.Answer, TestId: msg.TestId}
+		if err := s.repo.SetQuestion(stream.Context(), qa); err != nil {
+			return err
+		}
+	}
 }
