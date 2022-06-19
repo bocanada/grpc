295db2b42302474c2b821b13b37526955822b375
Mauro M <24578415+Bocanada@users.noreply.github.com>
2022-06-19 20:00:28 -0300

    add abstract methods for Test

--- repository/repository.go
+++ repository/repository.go
@@ -9,6 +9,8 @@ import (
 type Respository interface {
 	GetStudent(ctx context.Context, id string) (*models.Student, error)
 	SetStudent(ctx context.Context, student *models.Student) error
+	GetTest(ctx context.Context, id string) (*models.Test, error)
+	SetTest(ctx context.Context, test *models.Test) error
 }
 
 var impl Respository
@@ -23,4 +25,12 @@ func GetStudent(ctx context.Context, id string) (*models.Student, error) {
 
 func SetStudent(ctx context.Context, student *models.Student) error {
 	return impl.SetStudent(ctx, student)
+}
+
+func GetTest(ctx context.Context, id string) (*models.Test, error) {
+	return impl.GetTest(ctx, id)
+}
+
+func SetTest(ctx context.Context, test *models.Test) error {
+	return impl.SetTest(ctx, test)
 }
